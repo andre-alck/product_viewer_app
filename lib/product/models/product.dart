9908ed3f102c6956eb8e1f1dfc35fbc513f03ec0
andre-alck <andrealck1@gmail.com>
2022-10-21 12:28:20 -0300

    feat: add product model

---
+++ lib/product/models/product.dart
@@ -0,0 +1,11 @@
+class Product {
+  final double price;
+  final String imageURL;
+  final String description;
+
+  const Product({
+    required this.price,
+    required this.imageURL,
+    required this.description,
+  });
+}
