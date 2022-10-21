e79122539c75e646b17dd3d912543f841c52515f
andre-alck <andrealck1@gmail.com>
2022-10-21 15:03:13 -0300

    feat: adapt widget to show product

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,12 @@ class MyApp extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+      ),
       title: 'Product Viewer',
-      home: Home(),
+      home: const Home(),
     );
   }
 }
@@ -33,16 +36,76 @@ class Home extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    const product = Product(
-      price: 1.0,
-      imageURL:
-          'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
-      description: 'foo',
+    List<Product> products = [];
+    List<double> prices = [];
+    List<String> imageURLs = [];
+    List<String> description = [];
+
+    prices.addAll(
+      [
+        1.0,
+        2.0,
+        3.0,
+        4.0,
+        5.0,
+        6.0,
+        7.0,
+        8.0,
+      ],
     );
 
-    return const Scaffold(
-      body: ShowProduct(
-        product: product,
+    imageURLs.addAll(
+      [
+        'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
+        'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
+        'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
+        'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
+        'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
+        'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
+        'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
+        'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
+      ],
+    );
+
+    description.addAll(
+      [
+        'foo:bar',
+        'foo:bar',
+        'foo:bar',
+        'foo:bar',
+        'foo:bar',
+        'foo:bar',
+        'foo:bar',
+        'foo:bar',
+      ],
+    );
+
+    for (int i = 0; i < prices.length; i++) {
+      products.add(
+        Product(
+          price: prices[i],
+          imageURL: imageURLs[i],
+          description: description[i],
+        ),
+      );
+    }
+
+    return Scaffold(
+      body: ListView.separated(
+        separatorBuilder: (
+          context,
+          index,
+        ) =>
+            const Divider(),
+        itemCount: products.length,
+        itemBuilder: (
+          BuildContext context,
+          int index,
+        ) {
+          return ShowProduct(
+            product: products[index],
+          );
+        },
       ),
     );
   }
--- lib/product/widgets/show_product.dart
+++ lib/product/widgets/show_product.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:product_viewer/product/models/product.dart';
+
+import '../models/product.dart';
 
 class ShowProduct extends StatelessWidget {
   final Product product;
-
   const ShowProduct({
     super.key,
     required this.product,
@@ -13,62 +13,15 @@ class ShowProduct extends StatelessWidget {
   Widget build(
     BuildContext context,
   ) {
-    var deviceHeight = MediaQuery.of(
-          context,
-        ).size.height /
-        4;
-    var deviceWidth = MediaQuery.of(
-          context,
-        ).size.width /
-        2;
-
-    return Padding(
-      padding: const EdgeInsets.all(
-        8.0,
+    return ListTile(
+      leading: Image.network(
+        product.imageURL,
       ),
-      child: Stack(
-        alignment: Alignment.bottomCenter,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: const BorderRadius.all(
-                Radius.circular(
-                  15.0,
-                ),
-              ),
-              color: Colors.grey[300],
-            ),
-            width: deviceWidth,
-            height: deviceHeight,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(
-                  product.description,
-                ),
-                Text(
-                  product.price.toString(),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: deviceWidth,
-            height: deviceHeight / 2,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(
-                Radius.circular(
-                  15.0,
-                ),
-              ),
-              color: Colors.blue,
-            ),
-            child: Image.network(
-              'https://storage.googleapis.com/cms-storage-bucket/780e0e64d323aad2cdd5.png',
-            ),
-          ),
-        ],
+      title: Text(
+        product.description,
+      ),
+      subtitle: Text(
+        product.price.toString(),
       ),
     );
   }
